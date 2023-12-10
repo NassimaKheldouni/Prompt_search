1b5fa2e7a3ada3c3b0d2bafde5d3b96d5f2ce7a4
Nassima <54705839+NassimaKheldouni@users.noreply.github.com>
2023-12-10 17:48:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
 import streamlit as st
 import pandas as pd
- 
-st.write("""
-# My first app
-Hello *world!*
-""")
+
+# Function to filter the table based on keywords
+def filter_table(df, keywords):
+    filtered_df = df[df.apply(lambda row: any(keyword in str(row) for keyword in keywords), axis=1)]
+    return filtered_df
+
+# Function to read Excel file from SharePoint (replace with your SharePoint URL)
+def read_excel_from_sharepoint(file_url):
+    # Use appropriate method to read the Excel file from SharePoint
+    # Example: df = pd.read_excel(file_url)
+    # Replace the above line with your SharePoint file reading logic
+    df = pd.DataFrame()  # Placeholder for demonstration
+    return df
+
+# Main Streamlit app
+def main():
+    st.title("Search App")
+
+    # Get user input for keywords
+    keywords = st.text_input("Enter keywords (comma-separated):")
+    keywords = [keyword.strip() for keyword in keywords.split(',')]
+
+    # Read Excel file from SharePoint (replace with your SharePoint URL)
+    sharepoint_url = "https://your-sharepoint-url.com/your-excel-file.xlsx"
+    df = read_excel_from_sharepoint(sharepoint_url)
+
+    # Filter the table based on keywords
+    if keywords:
+        filtered_df = filter_table(df, keywords)
+        st.write("Filtered Table:")
+        st.write(filtered_df)
+    else:
+        st.warning("Please enter keywords to search.")
+
+if __name__ == "__main__":
+    main()
